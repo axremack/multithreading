a0fb1df57f646c89d01d75b120c9c50c7c5b2485
uke <ax.remack@gmail.com>
2021-11-23 07:58:20 +0100

    :sparkles: Making StudentGenerator runnable and implementing threaded generation

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Main {
     public static final int NUMBER_OF_THREADS = 3;
     public static Thread[] threads = new Thread[NUMBER_OF_THREADS];
+    public static StudentGenerator sg;
 
     public static void main(String[] args) {
         // Testing student generation
@@ -29,7 +30,7 @@ public class Main {
     public static long sequentialGeneration(int nb){
         long start_time = System.nanoTime();
 
-        ArrayList<Student> list = StudentGenerator.generate(nb);
+        ArrayList<Student> list = sg.generate(nb);
 
         long end_time = System.nanoTime();
         long diff = end_time - start_time;
@@ -39,15 +40,13 @@ public class Main {
 
     public static long threadedGeneration() throws InterruptedException {
         for (int i = 0; i < NUMBER_OF_THREADS; i++){
-            threads[i] = new Thread(new StudentGenerator());
+            threads[i] = new Thread(sg);
             threads[i].start();
         }
 
         for (int i = 0; i < NUMBER_OF_THREADS; i++){
-            System.out.println(i);
             threads[i].join();
         }
-
 
         return 1;
     }
--- src/StudentGenerator.java
+++ src/StudentGenerator.java
@@ -18,10 +18,15 @@ public class StudentGenerator implements Runnable {
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        int nb = 50000;
+        for (int i = 0; i < nb; i++) {
+            Random rand = new Random();
+            int random_id = rand.nextInt(nb);
+            Student s = new Student(random_id);
+
+            synchronized (this) {
+                list.add(s);
+            }
         }
     }
 }
