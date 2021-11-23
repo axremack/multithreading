30a7cc308c56f6b4c037e0f7c38dbfd0d2fdcce9
uke <ax.remack@gmail.com>
2021-11-23 08:31:48 +0100

    :sparkles: Improving sequential performance mesure by using mean

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,55 @@ import java.util.ArrayList;
 
 public class Main {
     public static final int NUMBER_OF_THREADS = 3;
+    public static final int ITERATIONS = 5;
     public static Thread[] threads = new Thread[NUMBER_OF_THREADS];
-    public static StudentGenerator sg;
+    public static StudentGenerator sg_sequential = new StudentGenerator();
+    public static StudentGenerator sg_thread = new StudentGenerator();
+
 
     public static void main(String[] args) {
-        // Testing student generation
-        /*StudentGenerator sg = new StudentGenerator();
-        ArrayList<Student> list = StudentGenerator.generate(20);
-        for (Student s : list) {
-            s.print();
-        }*/
+        // Sequential generation performance
+        long sequential_perf = 0;
 
-        // Sequential generation of 150000 students
-        long seq_time = sequentialGeneration(150000);
-        System.out.println("Temps d'éxécution : " + (seq_time / 1e9) + " secondes.");
-
-        // Threaded generation of 150000 students
-        try {
-            long thread_time = threadedGeneration();
-            System.out.println("Temps d'éxécution : " + (thread_time / 1e9) + " secondes.");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < ITERATIONS; i++) {
+            sequential_perf += sequentialGeneration(150000);
         }
 
+        sequential_perf /= ITERATIONS;
+        System.out.println("Temps d'éxécution séquentielle moyen : " + (sequential_perf) + " millisecondes.\n");
 
 
+        // Threaded generation performance
+        /*long threaded_perf = 0;
+
+        try {
+            for (int i = 0; i < ITERATIONS; i++) {
+                threaded_perf += threadedGeneration();
+            }
+
+            //threaded_perf /= ITERATIONS;
+            System.out.println("Temps d'éxécution multithreadé moyen : " + (threaded_perf) + " millisecondes.\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
     }
 
     public static long sequentialGeneration(int nb){
-        long start_time = System.nanoTime();
+        long start_time = System.currentTimeMillis();
 
-        ArrayList<Student> list = sg.generate(nb);
+        sg_sequential.generate(nb);
 
-        long end_time = System.nanoTime();
+        long end_time = System.currentTimeMillis();
         long diff = end_time - start_time;
 
         return diff;
     }
 
     public static long threadedGeneration() throws InterruptedException {
-        long start_time = System.nanoTime();
+        long start_time = System.currentTimeMillis();
 
         for (int i = 0; i < NUMBER_OF_THREADS; i++){
-            threads[i] = new Thread(sg);
+            threads[i] = new Thread(sg_thread);
             threads[i].start();
         }
 
@@ -52,7 +58,7 @@ public class Main {
             threads[i].join();
         }
 
-        long end_time = System.nanoTime();
+        long end_time = System.currentTimeMillis();
         long diff = end_time - start_time;
 
         return diff;
